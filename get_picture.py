fb49d848cccbc0bfbf454410ae9cd0c232e35eb1
Eclips4 <80244920+Eclips4@users.noreply.github.com>
2021-03-19 14:20:37 +0200

    f*ckin' refactoring

--- get_picture.py
+++ get_picture.py
@@ -6,8 +6,6 @@ from bs4 import BeautifulSoup as bs
 class Picture:
     def __init__(self, link):
         self.link = link
-        self.links = []
-        self.images = []
 
     def get_count_pages(self):
         mainpage = requests.get(self.link)
@@ -18,16 +16,14 @@ class Picture:
         return get_art_page
 
     def get_links(self):
-        parse_page = self.get_count_pages()
-        parse_art_page = bs(parse_page.text, 'html.parser')
+        parse_art_page = bs(self.get_count_pages().text, 'html.parser')
         links_page = parse_art_page.findAll(class_='link')
-        links = random.choice([tag['href'] for tag in links_page])
+        links = [tag['href'] for tag in links_page]
         link = random.choice(links)
         return link
 
     def get_pictures(self):
-        img_post_link = requests.get(f'http://anime.reactor.cc{self.get_links()}')
-        images = bs(img_post_link.text, 'html.parser')
+        images = bs(requests.get(f'http://anime.reactor.cc{self.get_links()}').text, 'html.parser')
         class_image = images.findAll(class_='image')
         images_links = [i.__str__()[i.__str__().find('src="') + 5:i.__str__().find(' ', i.__str__().find('src="')) - 1]
                         for i in class_image]
